b9ddaf0c8005c3fb7328df22d0ee7559c1d61723
pl-jay <lakshan.jap@gmail.com>
2020-02-27 01:56:51 +0530

    Third Commit

--- HTTPClient/src/lk/pathum/HTTPClient.java
+++ HTTPClient/src/lk/pathum/HTTPClient.java
@@ -1,27 +1,120 @@
 package lk.pathum;
 
-import java.net.URI;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
 public class HTTPClient {
 
+    private static boolean isConnected = false;
+    private static String username;
+    private static String serverUrl;
+    private static HttpURLConnection con;
+
+
     public static void main(String[] args) {
 
-        while (true){
-            HttpClient client = HttpClient.newHttpClient();
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(URI.create("http://localhost:8590/recieve-messages"))
-                    .build();
-            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-                    .thenApply(HttpResponse::body)
-                    .thenAccept(System.out::println)
-                    .join();
+        Scanner input = new Scanner(System.in);
+
+        while(true){
+            try{
+                String command = input.nextLine();
+                if(!isConnected){
+                    connectToServer();
+                }else if(command.matches("send .* -> .*") && isConnected) {
+                    String[] sub = command.substring(5).split(" -> ");
+                    sendMessage(sub[0],sub[1]);
+                } else if(command.equals("list") && isConnected){
+                    listUsers();
+                }
+
+                messageRequest();
+            } catch (Exception e){
+                e.printStackTrace();
+            }
         }
+    }
 
+    //abc=> [/127.0.0.1:56102 /127.0.0.1:56104]  abcd=> [/127.0.0.1:56106 /127.0.0.1:56108]
 
+    private static void messageRequest(){
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://"+serverUrl+"/connectToserver"))
+                .POST(HttpRequest.BodyPublishers.ofString(username))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+    }
 
+    private static void listUsers(){
+
+        var url = "http://"+serverUrl+"/listUsers";
+
+        try {
+
+            var myurl = new URL(url);
+            con = (HttpURLConnection) myurl.openConnection();
+            con.setRequestMethod("GET");
+            List<String> content = new ArrayList<>();
+            try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
+                String line;
+                while ((line = in.readLine()) != null) {
+                    content.add(line.toString());
+                }
+            }
+            content.forEach(System.out::print);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            con.disconnect();
+        }
+    }
+
+    private static void connectToServer(){
+        Scanner inputS = new Scanner(System.in);
+        System.out.println("Type 'connect <host address>:<port> as <username>' ");
+        String hostPc = inputS.nextLine();
+        String[] split = hostPc.split(" ", 4);
+        serverUrl = split[1];
+        username = split[3];
+        //inputS.close();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://"+serverUrl+"/connectToserver"))
+                .POST(HttpRequest.BodyPublishers.ofString(username))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
+        isConnected = true;
+    }
+
+    private static void sendMessage(String message,String to){
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://"+serverUrl+"/sendMessage"))
+                .POST(HttpRequest.BodyPublishers.ofString(to+" "+"["+username+"]"+":"+message))
+                .build();
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(System.out::println)
+                .join();
     }
 }
--- HTTPServer/src/lk/pathum/Main.java
+++ HTTPServer/src/lk/pathum/Main.java
@@ -2,20 +2,24 @@ package lk.pathum;
 
 import com.sun.net.httpserver.*;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.URI;
+import java.io.*;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 
 public class Main {
+
+    private static HashMap<String, InetSocketAddress> users = new HashMap<>();
 
     public static void main(String[] args){
         try{
             HttpServer server = HttpServer.create(new InetSocketAddress(8590),0);
             server.createContext("/").setHandler(Main::handleRequest);
-            server.createContext("/recieve-messages").setHandler(Main::incomingMessages);
-
+            server.createContext("/sendMessage").setHandler(Main::incomingMessages);
+            server.createContext("/connectToserver").setHandler(Main::connectToServer);
+            server.createContext("/listUsers").setHandler(Main::usersList);
 
             server.start();
 
@@ -35,9 +39,35 @@ public class Main {
         os.close();
     }
 
+
+    private static void usersList(HttpExchange exchange) throws IOException {
+        URI requestURI = exchange.getRequestURI();
+
+        String respone = users.entrySet().toString();
+        exchange.sendResponseHeaders(200, respone.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(respone.getBytes());
+        System.out.println(respone.getBytes().toString());
+        os.close();
+    }
+
+    private static void connectToServer(HttpExchange exchange) throws IOException {
+        URI requestURI = exchange.getRequestURI();
+        users.put(getRequestBody(exchange),exchange.getRemoteAddress());
+
+        String respone = String.valueOf(exchange.getRemoteAddress().getPort());
+        System.out.println(respone);
+        exchange.sendResponseHeaders(200, respone.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(respone.getBytes());
+
+        os.close();
+    }
+
     private static void incomingMessages(HttpExchange exchange) throws IOException {
         URI requestURI = exchange.getRequestURI();
-        printRequestInfo(exchange);
+        sendMessage(getRequestBody(exchange));
+
         String respone = "Helo";
         exchange.sendResponseHeaders(200, respone.getBytes().length);
         OutputStream os = exchange.getResponseBody();
@@ -63,5 +93,58 @@ public class Main {
         URI requestURI = exchange.getRequestURI();
         String query = requestURI.getQuery();
         System.out.println(query);
+    }
+
+    private static String getRequestBody(HttpExchange exchange) throws IOException{
+        String requestBody;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(exchange.getRequestBody(), "utf-8"))) {
+            requestBody = br.lines().collect(Collectors.joining(System.lineSeparator()));
+        }
+
+        return requestBody;
+    }
+
+    private static boolean sendMessage(String message){
+
+        HttpURLConnection con = null;
+        var url = "https:/"+addressForUser(message.split(" ",2)[0]);
+        System.out.println(url);
+        var urlParameters = message.split(" ",2)[1];
+        byte[] postData = urlParameters.getBytes(StandardCharsets.UTF_8);
+        try {
+            var myurl = new URL(url);
+            con = (HttpURLConnection) myurl.openConnection();
+            con.setDoOutput(true);
+            con.setRequestMethod("POST");
+            con.setRequestProperty("User-Agent", "Java client");
+            con.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            try (var wr = new DataOutputStream(con.getOutputStream())) {
+                wr.write(postData);
+            }
+            StringBuilder content;
+            try (var br = new BufferedReader(
+                    new InputStreamReader(con.getInputStream()))) {
+                String line;
+                content = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    content.append(line);
+                    content.append(System.lineSeparator());
+                }
+            }
+            System.out.println(content.toString());
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            con.disconnect();
+        }
+        return false;
+    }
+
+    private static String addressForUser(String user){
+        return users.get(user).getAddress()+":"+users.get(user).getPort();
     }
 }
